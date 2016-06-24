5a803b9fa5916efb3f1cb87efdf7cb2a94063fa8
Ewerton Belchior <ewerton.belchior@cern.ch>
2016-06-24 20:46:09 -0300

    data types

---
+++ Formats/interface/DataTypes.h
@@ -0,0 +1,41 @@
+#ifndef Formats_DataTypes_h
+#define Formats_DataTypes_h
+
+// user include files
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/Common/interface/View.h"
+#include "FastSimulation/PPSFastObjects/interface/PPSSpectrometer.h"
+#include "FastSimulation/PPSFastObjects/interface/PPSGenData.h"
+#include "FastSimulation/PPSFastObjects/interface/PPSSimData.h"
+#include "FastSimulation/PPSFastObjects/interface/PPSRecoData.h"
+
+// class declaration
+class DataTypes {
+
+  public:
+
+    // constructors and destructors
+    DataTypes(){};
+    ~DataTypes(){};
+
+    // typedefs
+    typedef reco::Vertex Vertex;
+    typedef reco::GenParticle GenParticle;
+    typedef reco::GenJet GenJet;
+    typedef pat::Jet Jet;
+    typedef reco::Track Track;
+    typedef edm::View<Vertex> VertexCollection;
+    typedef edm::View<GenParticle> GenParticleCollection;
+    typedef edm::View<GenJet> GenJetCollection;
+    typedef edm::View<Jet> JetCollection;
+    typedef edm::View<Track> TrackCollection;
+    typedef PPSSpectrometer<PPSGenData> PPSGenCollection;
+    typedef PPSSpectrometer<PPSSimData> PPSSimCollection;
+    typedef PPSSpectrometer<PPSRecoData> PPSRecoCollection;
+};
+
+#endif
---
+++ Formats/src/classes.h
@@ -0,0 +1,14 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+#include <utility>
+
+namespace WWAnalysis_WWUtils {
+
+   struct dictionary {
+
+      // CTPPSTools/Filters/interface/DoubleArmFilter.cc
+      std::pair<size_t,size_t> dummy0;
+      std::vector<std::pair<size_t,size_t> > dummy1;
+      edm::Wrapper<std::vector<std::pair<size_t,size_t> > > dummy2;
+   };
+}
